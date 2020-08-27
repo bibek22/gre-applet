5c6250be66f2c78fc79395788eaba12b4414c9e4
Bibek Gautam <forgot.bg@gmail.com>
2020-08-27 10:30:45 +0545

    Works well enough for now.

--- button.py
+++ button.py
@@ -12,18 +12,91 @@ ht = 1
 w = 6
 i = 1
 
-def show(answers):
+class Response(object):
+
+    """response object ie. answer from test taker"""
+
+    def __init__(self, qn):
+        self.qn = qn
+        self.answer = None
+        self.time = None
+        self.key = None
+        self.result = None
+
+    def get_duration(self):
+        time = str(self.time // 60) + ":" + "{:02d}".format(self.time % 60)
+        return(time)
+
+
+class Section(object):
+
+    """collection of responses"""
+
+    def __init__(self):
+        self.questions = []
+        self.critical_time = 142
+        self.keys = None
+
+    def add_question(self, qn):
+        new = Response(qn)
+        self.questions.append(new)
+        return(new)
+
+    def read_answers(self):
+        # get rid of the last question right away
+        del self.questions[-1]
+        print(f"total responses: {len(self.questions)}")
+        self.keys = input("Answers: ").strip().replace(" ", "").upper()
+        total_keys = len(self.keys)
+        for i, q in enumerate(self.questions):
+            #  if not q.answer: del self.questions[i]
+            if q.qn > total_keys: print("not all answerkeys read..."); break
+            q.key = self.keys[q.qn-1]
+        
+    def prepare_result(self):
+        for q in self.questions:
+            if not q.answer: continue
+            if q.answer == q.key:
+                q.result = 1
+            else:
+                q.result = 0
+
+    def show_response(self):
+        banner = "Qn.\tAns.\tTime"
+        print("\n" + "="*len(banner))
+        print(banner)
+        print("-"*len(banner))
+        for q in self.questions:
+            print("%s\t%s\t%s" % (q.qn, q.answer, q.get_duration()))
+
+
+    def show_result(self):
+        banner = "Qn.\tAnswer\tTime"
+        print("\n" + "="*len(banner))
+        print(banner)
+        print("-"*len(banner))
+        for q in self.questions:
+            if q.result:
+                print("%s\t%s\t\t%s" % (q.qn, q.answer, q.get_duration()))
+            else:
+                print("%s\t%s(%s)\t%s" % (q.qn, q.answer, q.key, q.get_duration()))
+
+
+def show(responses):
     banner = "Qn.\tAns\tTime"
     print("\n" + "="*len(banner))
     print(banner)
     print("-"*len(banner))
-    for item in answers:
+    for item in responses:
         print("%s\t%s\t%s" % item)
 
-#  sg.theme('DarkGreen3')	# Add a touch of color
+def read_answers():
+    answers = input("Answers: ").strip().replace(" ", "")
+    return answers
+    
+
 sg.theme('DarkGreen2')	# Add a touch of color
 # All the stuff inside your window.
-the_only_button = sg.Button("Qn: {}".format(i), size=(2*w+5, 2*ht+4))
 field = sg.Text(f" Qn: {i}", size=(w, 2), font=font, justification='center')
 options = ["A", "B", "C", "D", "E", "F", "G"]
 option_buttons = [sg.Button(label, size=(w, ht), font=font) for label in options]
@@ -51,19 +124,26 @@ def timer(field):
         label = sec2time(duration)
         field.Update()
 
+# create a section object
+section = Section()
 # Create the Window
 window = sg.Window('record', layout, finalize=True)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
+    question = section.add_question(i)
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel':# if user closes window or clicks cancel
         break
     if event in options:
         now = time.time() 
-        duration = sec2time(int(now - prev))
-        answers.append((i, event, duration))
+        question.time = int(now - prev)
+        question.answer = event
         i+=1
         field.Update(f" Qn: {i}")
         prev = now
+
 window.close()
-show(answers)
+section.read_answers()
+section.prepare_result()
+section.show_result()
+#  section.show_response()
